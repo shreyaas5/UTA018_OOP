3a7dda5a8f8dc7d24bc8025c9f57d7da49927327
shreyaas5 <ssachdeva1_be24@thapar.edu>
2025-09-17 03:03:52 +0530

    Create Part-1.cpp

---
+++ Question Bank/Object and Class/Q1. Calendar/Part-1.cpp
@@ -0,0 +1,8 @@
+//Write the class definition for a Date class that contains three private integer data members:
+// month, day, and year. Create a static member to hold a slash. Create two public member
+// functions, setDate() and showDate(). You will use the static slash in the showDate() function.
+// The setDate()function accepts three integer arguments and passes them on to three private
+// functions, setMonth(), setDay(), and setYear(). If a month is greater than 12, then set it to 12.
+// If a day is greater than 31, then set it to 31. Write a main()function that instantiates several
+// objects of the Date class and tests the member functions.
+
